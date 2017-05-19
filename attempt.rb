98391bc4f776d6134e412135898d52814a1e4ff3
deadDrifters <nebyoung@ksu.edu>
2017-05-19 13:45:00 -0700

    Update attempt.rb

--- attempt.rb
+++ attempt.rb
@@ -1,16 +1,29 @@
 execute "download" do
-	unless File.file?("~/Nessus")
-		cwd ~/Nessus/
-		command "sudo wget https://github.com/deadDrifters/EST-Nessus/raw/master/Nessus_ -P ~/Nessus/"
-	end
-end 
+        #unless File.file?("~/Nessus/Nessus.deb")
+        unless  ::File.file?("~/Nessus/Nessus_")
+                cwd ~/Nessus/
+                command "sudo wget https://github.com/deadDrifters/EST-Nessus/raw/master/Nessus_ -P ~/Nes$
+                #command "sudo wget  -O ~/Nessus/ \"http://downloads.nessus.org/nessus3dl.php?file=Nessus$
+        end
+end
 execute "install" do
-	unless File.file?("~/Nessus")
-		command "sudo dpkg -i ~/Nessus/Nessus_"
-	end
+        unless  ::File.file?("~/Nessus/Nessus_")
+                command "sudo dpkg -i ~/Nessus/Nessus_"
+                action :run
+        end
 end
 execute "install-configure" do
-	unless File.file?("~/Nessus")
-		command "sudo apt-get install -f"
-	end
+        unless  ::File.file?("~/Nessus/Nessus_")
+                command "sudo apt-get install -f"
+        end
 end
+execute "Disable auto-start" do
+        unless  ::File.file?("~/Nessus/Nessus_")
+                command "sudo update-rc.d nessusd disable"
+        end
+end
+execute "Start" do
+        command "sudo service nessusd start"
+end
+
+
