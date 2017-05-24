c3c21ac6d64a61553047a05e84159d716c7c0510
skytap <skytap@host-1.skytap.example>
2017-05-24 13:11:24 -0700

    dependencies

---
+++ attempt2.rb
@@ -0,0 +1,35 @@
+execute "update" do
+	command "sudo apt-get update"
+end
+execute "dependencies" do
+	command "sudo apt-get -f install"
+end
+execute "download" do
+        #unless File.file?("~/Nessus/Nessus.deb")
+        unless  ::File.file?("~/Nessus/Nessus_")
+                cwd ~/Nessus/
+                command "sudo wget https://github.com/deadDrifters/EST-Nessus/raw/master/Nessus_ -P ~/Nessus/"
+                #command "sudo wget  -O ~/Nessus/ \"http://downloads.nessus.org/nessus3dl.php?file=Nessus""
+        end
+end
+execute "install" do
+        unless  ::File.file?("~/Nessus/Nessus_")
+                command "sudo dpkg -i ~/Nessus/Nessus_"
+                action :run
+        end
+end
+execute "install-configure" do
+        unless  ::File.file?("~/Nessus/Nessus_")
+                command "sudo apt-get install -f"
+        end
+end
+execute "Disable auto-start" do
+        unless  ::File.file?("~/Nessus/Nessus_")
+                command "sudo update-rc.d nessusd disable"
+        end
+end
+execute "Start" do
+        command "sudo service nessusd start"
+end
+
+
