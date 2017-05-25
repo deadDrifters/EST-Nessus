445feb0ccce9143fbe20df3b721940227482b057
ben <nebyoung@yahoo.com>
2017-05-25 10:59:33 -0500

    permissions

--- attempt2.rb
+++ attempt2.rb
@@ -1,35 +1,39 @@
 execute "update" do
-	command "sudo apt-get update"
+        command "apt-get update"
 end
 execute "dependencies" do
-	command "sudo apt-get -f install"
+        command "apt-get -f install"
 end
 execute "download" do
         #unless File.file?("~/Nessus/Nessus.deb")
         unless  ::File.file?("~/Nessus/Nessus_")
                 cwd ~/Nessus/
-                command "sudo wget https://github.com/deadDrifters/EST-Nessus/raw/master/Nessus_ -P ~/Nessus/"
+                command "wget https://github.com/deadDrifters/EST-Nessus/raw/master/Nessus_ -P ~/Nessus/"
                 #command "sudo wget  -O ~/Nessus/ \"http://downloads.nessus.org/nessus3dl.php?file=Nessus""
         end
 end
+execute "dependencies" do
+        command "apt-get -f install"
+end
 execute "install" do
         unless  ::File.file?("~/Nessus/Nessus_")
-                command "sudo dpkg -i ~/Nessus/Nessus_"
+                command "dpkg -i ~/Nessus/Nessus_; apt-get -f install"
                 action :run
         end
 end
 execute "install-configure" do
         unless  ::File.file?("~/Nessus/Nessus_")
-                command "sudo apt-get install -f"
+                command "apt-get install -f"
         end
 end
 execute "Disable auto-start" do
         unless  ::File.file?("~/Nessus/Nessus_")
-                command "sudo update-rc.d nessusd disable"
+                command "update-rc.d nessusd disable"
         end
 end
 execute "Start" do
-        command "sudo service nessusd start"
+        command "service nessusd start"
 end
-
-
+execute "Stop" do
+	command "service nessusd stop"
+end
