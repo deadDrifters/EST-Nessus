7f415435e18c67ac423a59f6714f15bc27ea97d8
ben <nebyoung@yahoo.com>
2017-05-25 15:46:00 -0500

    adduser?

--- attempt2.rb
+++ attempt2.rb
@@ -1,12 +1,15 @@
+require "pty"
+require "expect"
+
 execute "update" do
-        command "apt-get update"
+        command "apt-get update; sudo dpkg --configure -a"
 end
 execute "dependencies" do
         command "apt-get -f install"
 end
 execute "download" do
         #unless File.file?("~/Nessus/Nessus.deb")
-        unless  ::File.file?("~/Nessus/Nessus_")
+	if ::File.file?("~/Nessus/Nessus_/")
                 cwd ~/Nessus/
                 command "wget https://github.com/deadDrifters/EST-Nessus/raw/master/Nessus_ -P ~/Nessus/"
                 #command "sudo wget  -O ~/Nessus/ \"http://downloads.nessus.org/nessus3dl.php?file=Nessus""
@@ -16,24 +19,40 @@ execute "dependencies" do
         command "apt-get -f install"
 end
 execute "install" do
-        unless  ::File.file?("~/Nessus/Nessus_")
+        if  ::File.file?("~/Nessus/Nessus_/")
                 command "dpkg -i ~/Nessus/Nessus_; apt-get -f install"
                 action :run
         end
 end
 execute "install-configure" do
-        unless  ::File.file?("~/Nessus/Nessus_")
+        if  ::File.file?("~/Nessus/Nessus_/")
                 command "apt-get install -f"
         end
 end
 execute "Disable auto-start" do
-        unless  ::File.file?("~/Nessus/Nessus_")
+        if  !::File.file?("~/Nessus/Nessus_")
                 command "update-rc.d nessusd disable"
         end
 end
 execute "Start" do
         command "service nessusd start"
 end
-execute "Stop" do
-	command "service nessusd stop"
+execute "nessus_Update" do
+	command "sudo ../../../opt/nessus/sbin/nessuscli update; sudo service nessusd stop; sudo ../../../opt/nessus/sbin/nessuscli update"
 end
+#execute "add_user" do
+#	command "sudo ../../../opt/nessus/sbin/nessuscli adduser Scanner"
+#	echo "password\n"
+#	echo "y\n"
+#end
+PTY.spawn("sudo ../../../opt/nessus/sbin/nessuscli adduser Scanner") do |reader, writer|
+	reader.expect("Login password: ", 5)
+	writer.puts("password")
+	reader.expect("y/n")
+	writer.puts("y")
+end
+
+
+#execute "Stop" do
+#	command "service nessusd stop"
+#end
